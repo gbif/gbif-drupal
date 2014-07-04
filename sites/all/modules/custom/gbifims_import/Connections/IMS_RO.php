2404ed70a787205e75a1e3c259728c88aab7e2a4
BurkeChih-JenKo <bko@gbif.org>
2014-07-04 15:44:56 +0200

    IMS credentials should be retrieved from local settings.php.

--- sites/all/modules/custom/gbifims_import/Connections/IMS_RO.php
+++ sites/all/modules/custom/gbifims_import/Connections/IMS_RO.php
@@ -7,9 +7,10 @@
 // set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 require_once(dirname(__FILE__).'/../FileMaker.php');
 require_once(dirname(__FILE__).'/../FileMaker/FMStudio_Tools.php');
-$hostname_IMS_RO = "";
-$database_IMS_RO = "";
-$username_IMS_RO = "";
-$password_IMS_RO = "";
+$ims = variable_get('ims_connection');
+$hostname_IMS_RO = $ims['hostname'];
+$database_IMS_RO = $ims['database'];
+$username_IMS_RO = $ims['username'];
+$password_IMS_RO = $ims['password'];
 $IMS_RO = new FileMaker($database_IMS_RO, $hostname_IMS_RO, $username_IMS_RO, $password_IMS_RO); 
 ?>

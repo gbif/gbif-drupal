20f7d71637fd2fc0ed2d7938087d05579be7e038
Burke Chih-Jen Ko <burkeker@gmail.com>
2014-09-05 00:12:40 +0200

    Custom template.

---
+++ sites/all/modules/custom/gbifs/gbif_participant/theme/summary.tpl.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @file
+ *
+ *
+ * @ingroup themeable
+ */
+?>
+<article id="country-summary" class="container <?php print $classes; ?>">
+	<div class="row">
+      <section id="participation" class="col-md-12 well well-lg well-margin-top clearfix"<?php print $attributes; ?>>
+        <div class="row">
+					<header class="content-header col-md-8">
+						<h2>Map is here</h2>
+					</header>
+					<div class="node-sidebar-header col-md-4">
+						<h2>Data about</h2>
+					</div>
+        </div>
+        <div class="row">
+          <div class="node-content col-md-8">
+          </div>
+          <aside class="node-sidebar col-md-4">
+						<div class="field-label">Node name&nbsp;</div>
+						<div class="field-label">Address&nbsp;</div>
+						<p>
+						</p>
+						<div class="field-label">Website&nbsp;</div>
+						<p></p>
+          </aside>
+        </div>
+      </section>
+			<section id="description" class="col-md-12 well well-lg">
+				<div class="row">
+					<header class="content-header col-md-12">
+						<h2>Description</h2>
+					</header>
+				</div>
+				<div class="row">
+					<div class="node-content col-md-8">
+					</div>
+					<aside class="node-sidebar col-md-4">
+					</aside>
+				</div>
+			</section>
+			<section id="contacts" class="col-md-12 well well-lg">
+				<div class="row">
+					<header class="content-header col-md-12">
+						<h2>Contacts</h2>
+					</header>
+				</div>
+				<div class="row">
+				</div>
+			</section>
+			<section id="endorsed-publishers" class="col-md-12 well well-lg">
+				<div class="row">
+					<header class="content-header col-md-12">
+						<h2>Endorsed Publishers</h2>
+					</header>
+				</div>
+				<div class="row">
+					<div class="node-content col-md-12">
+					</div>
+				</div>
+			</section>
+	</div>
+</article>

3d2b7116fe61be40efc00832b52b7df44186c50e
Burke Chih-Jen Ko <burkeker@gmail.com>
2014-09-07 20:02:13 +0200

    Chapter routing and templates.

---
+++ sites/all/modules/custom/gbifs/gbif_participant/theme/about.tpl.php
@@ -0,0 +1,55 @@
+<?php
+
+?>
+<article id="country-data-about" class="container">
+	<div class="row">
+      <section id="data-about" class="col-md-12 well well-lg well-margin-top clearfix"<?php print $attributes; ?>>
+        <div class="row">
+					<header class="content-header col-md-8">
+						<h2>Map: Data About</h2>
+					</header>
+					<header class="content-header sidebar-header-country col-md-4">
+						<h2>Data about</h2>
+					</header>
+        </div>
+        <div class="row">
+        </div>
+      </section>
+
+			<section id="largest-occurrence-datasets" class="col-md-12 well well-lg">
+				<div class="row">
+					<header class="content-header col-md-12">
+						<h2>Largest occurrence datasets about</h2>
+					</header>
+				</div>
+				<div class="row">
+					<div class="content col-md-12">
+					</div>
+				</div>
+			</section>
+
+			<section id="countries-publishing-data-about" class="col-md-12 well well-lg">
+				<div class="row">
+					<header class="content-header col-md-12">
+						<h2>Countries, territories or islands publishing data about</h2>
+					</header>
+				</div>
+				<div class="row">
+					<div class="content col-md-12">
+					</div>
+				</div>
+			</section>
+
+			<section id="occurrence-located-in" class="col-md-12 well well-lg">
+				<div class="row">
+					<header class="content-header col-md-12">
+						<h2>Occurrence located in</h2>
+					</header>
+				</div>
+				<div class="row">
+					<div class="content content-full col-md-12">
+					</div>
+				</div>
+			</section>
+	</div>
+</article>

e70680a3607535b2fb011bdb35d17ef408cee80f
Burke Chih-Jen Ko <burkeker@gmail.com>
2014-09-09 04:55:01 +0200

    Country list as entry page.

---
+++ sites/all/modules/custom/gbifs/gbif_participant/theme/countries.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<article id="countries" class="container">
+	<div class="row">
+		<section class="col-md-12 well well-lg well-margin-top">
+			<div class="content-header">
+				<p>Current GBIF Participants are marked with a *. A complete list of GBIF Participants, including organizations and economies, can be found <a href="/participation/list">here</a>.</p>
+			</div>
+			<div class="content content-country-list">
+				<?php print $country_list; ?>
+			</div>
+		</section>
+	</div>
+</article>

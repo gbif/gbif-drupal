70551a11550fc570f4ea503f85bb4f71bcfa347b
Burke Chih-Jen Ko <burkeker@gmail.com>
2014-09-11 22:35:47 +0200

    Repatriation on both about and publishing pages.

--- sites/all/modules/custom/gbifs/gbif_participant/theme/country-repatriation-list-item.tpl.php
+++ sites/all/modules/custom/gbifs/gbif_participant/theme/country-repatriation-list-item.tpl.php
@@ -3,14 +3,26 @@
 <?php	switch ($list_mode): ?>
 <?php	case 'digest': ?>
 		<?php
-			$title_link = l($country['title'], 'country/' . $iso2 . '/publishing');
-				$paragraph = format_plural($country['total_count'],
-					'1 occurrence',
-					'!count occurrences',
-					array('!count' => number_format($country['total_count']))
-				) . ', ';
-				// @todo georeferenced count.
-				$paragraph .= $country['percentage'] . t('% geo-referenced.');
+			if ($repatri_mode == 'from') {
+				$title_link = l($country['title'], 'country/' . $country['iso2']);
+			}
+			else {
+				$title_link = l($country['title'], 'country/' . $country['iso2'] . '/publishing');
+			}
+			$paragraph = format_plural($country['total_count'],
+				'1 occurrence',
+				'!count occurrences',
+				array('!count' => number_format($country['total_count']))
+			);
+			if ($country['percentage'] == 100) {
+				$paragraph .= ', ' . $country['percentage'] . t('% geo-referenced.');
+			}
+			elseif ($country['percentage'] == 0) {
+				$paragraph .= '.';
+			}
+			else {
+				$paragraph .= ', ' . number_format((float)$country['percentage'], 3, '.', '') . t('% geo-referenced.');
+			}
 		?>
 		<li>
 			<?php print $title_link; ?> <span class="title-annotating-text"><?php print $paragraph; ?></span>
@@ -18,17 +30,30 @@
 <?php break; ?>
 <?php case 'full': ?>
 		<?php
-			$title_link = l($country['title'], 'country/' . $iso2 . '/publishing');
+			if ($repatri_mode == 'from') {
+				$title_link = l($country['title'], 'country/' . $country['iso2'] . '/about');
+			}
+			else {
+				$title_link = l($country['title'], 'country/' . $country['iso2'] . '/publishing');
+			}
 			$paragraph = format_plural($country['total_count'],
 				'1 occurrence',
 				'!count occurrences',
 				array('!count' => number_format($country['total_count']))
-			) . ', ';
-			// @todo georeferenced count.
-			$paragraph .= $country['percentage'] . t('% geo-referenced.');
+			);
+			if ($country['percentage'] == 100) {
+				$paragraph .= ', ' . $country['percentage'] . t('% geo-referenced.');
+			}
+			elseif ($country['percentage'] == 0) {
+				$paragraph .= '.';
+			}
+			else {
+				$paragraph .= ', ' . number_format((float)$country['percentage'], 3, '.', '') . t('% geo-referenced.');
+			}
 		?>
 		<li>
-			<?php print $title_link; ?> <span class="title-annotating-text"><?php print $paragraph; ?></span>
+			<strong><?php print $title_link; ?></strong>
+			<p><?php print $paragraph; ?></p>
 		</li>
 <?php break; ?>
 <?php endswitch; ?>

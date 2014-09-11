3cadb45b778380b5f10c13486817e421c2ce5f63
Burke Chih-Jen Ko <bko@gbif.org>
2014-09-11 14:21:33 +0200

    Largest dataset list and link-out templates.

---
+++ sites/all/modules/custom/gbifs/gbif_participant/theme/about-datasets.tpl.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<div class="back-to-previous">
+	<div class="container">
+		<div class="row">
+			<p>&lt; <u><?php print $html['link_back']; ?></u></p>
+		</div>
+	</div>
+</div>
+<article id="country-publishers" class="container <?php print $classes; ?>">
+	<div class="row">
+      <section id="publishers" class="col-md-12 well well-lg well-margin-top clearfix"<?php print $attributes; ?>>
+        <div class="row">
+					<header class="content-header col-md-8">
+						<h2><?php print $html['largest_datasets_title']; ?></h2>
+					</header>
+        </div>
+        <div class="row">
+					<div class="content content-full">
+						<?php print $html['largest_datasets_list']; ?>
+					</div>
+        </div>
+      </section>
+	</div>
+</article>
---
+++ sites/all/modules/custom/gbifs/gbif_participant/theme/largest-datasets-list-item.tpl.php
@@ -0,0 +1,35 @@
+<ul>
+<?php foreach ($datasets as $k => $dataset): ?>
+		<?php
+			switch ($mode) {
+				case 'digest':
+					$title_link = l($dataset['title'], $env['data_portal_base_url'] . '/dataset/' . $dataset['dataset_key']);
+					$paragraph = format_plural($dataset['count_within_country'],
+							'1 occurrence',
+							'!count occurrences',
+							array('!count' => number_format($dataset['count_within_country']))
+						) . ' ';
+					$paragraph .= 'in ' . $dataset['country'] . ' ';
+					$paragraph .= t('out of');
+					break;
+				case 'full':
+					$title_link = '<strong>' . l($dataset['title'], $env['data_portal_base_url'] . '/dataset/' . $dataset['dataset_key']) . '</strong>';
+					$paragraph = $dataset['type_formatted'] . ' with ';
+					$occu_text = format_plural($dataset['count_within_country'],
+							'1 occurrence',
+							'!count occurrences',
+							array('!count' => number_format($dataset['count_within_country']))
+						) . ' ';
+					$paragraph .= l($occu_text, $env['data_portal_base_url'] . '/occurrence/search', array('query' => array('country' => $iso2, 'dataset_key' => $dataset['dataset_key'])));
+					$paragraph .= ' ' . t('in') . ' ' . $dataset['country'] . ' ' . t('out of');
+					break;
+			}
+			$paragraph .= ' ' . number_format($dataset['occurrence_count']) . ' ';
+			$paragraph .= '(' . $dataset['percentage'] . '%).';
+		?>
+	<li>
+		<?php print $title_link; ?><br>
+		<p><?php print $paragraph; ?></p>
+	</li>
+<?php endforeach; ?>
+</ul>

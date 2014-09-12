daf170a779dca34905530e726378f1d7c4496276
Burke Chih-Jen Ko <burkeker@gmail.com>
2014-09-12 22:05:16 +0200

    Styling of publications.

--- sites/all/modules/custom/gbifs/gbif_participant/theme/publications-list-item.tpl.php
+++ sites/all/modules/custom/gbifs/gbif_participant/theme/publications-list-item.tpl.php
@@ -13,20 +13,29 @@
 				}
 			}
 			$year = $p['year'];
-			$journal = '<em>' . $p['publication_outlet'] . '</em>';
-			$journal .= (!empty($p['volume'])) ? ' ' . $p['volume'] : '';
-			$journal .= (!empty($p['issue'])) ? '(' . $p['issue'] . ')' : '';
-			$journal .= (!empty($p['pages'])) ? ' ' . $p['pages'] . '.' : '. ';
+
+			if ($p['publication_outlet'] == '[Pending journal name]') {
+				$journal = '<em>' . t('(Journal name unavailable from Mendeley API. To be updated soon...)') . '</em>';
+			}
+			else {
+				$journal = '<em>' . $p['publication_outlet'] . '</em>';
+				$journal .= (!empty($p['volume'])) ? ' ' . $p['volume'] : '';
+				$journal .= (!empty($p['issue'])) ? '(' . $p['issue'] . ')' : '';
+				$journal .= (!empty($p['pages'])) ? ' ' . $p['pages'] . '.' : '. ';
+			}
 
 			// Keywords
-			$keywords = 'Keyword(s): ';
-			for ($i = 0; $i < count($p['keywords']); $i++) {
-				$keywords .= $p['keywords'][$i];
-				if ($i < count($p['keywords']) - 1) {
-					$keywords .= ', ';
-				}
-				elseif ($i == count($p['keywords'])) {
-					$keywords .= '.';
+			if (count($p['keywords']) > 0) {
+				$keywords = format_plural(count($p['keywords']),
+				'Keyword', 'Keywords', array()) . ': ';
+				for ($i = 0; $i < count($p['keywords']); $i++) {
+					$keywords .= $p['keywords'][$i];
+					if ($i < count($p['keywords']) - 1) {
+						$keywords .= ', ';
+					}
+					elseif ($i == count($p['keywords'])) {
+						$keywords .= '.';
+					}
 				}
 			}
 		?>
@@ -37,7 +46,7 @@
 			<p><?php print $p['abstract']; ?></p>
 			<p class="keywords"><?php print $keywords; ?></p>
 			<?php if ($k < count($publications) - 1): ?>
-			<hr>
+				<hr>
 			<?php endif; ?>
 		</li>
 	<?php endforeach; ?>

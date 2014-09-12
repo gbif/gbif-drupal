a97e2523ef50111b3a3b65280c2f4ca3368f65a6
Burke Chih-Jen Ko <burkeker@gmail.com>
2014-09-12 22:33:48 +0200

    Commentted out console.debug() for IE9 compatibility.

--- sites/all/modules/custom/gbifs/gbif_participant/js/loadMetrics.js
+++ sites/all/modules/custom/gbifs/gbif_participant/js/loadMetrics.js
@@ -16,50 +16,50 @@
 
     Drupal.behaviors.occMetrics = {
       attach: function (context, settings) {
-          $.fn.occMetrics = function(baseUrl){
-              this.each(function() {
-                  var baseAddress = $(this).attr("data-address");
-                  console.debug(baseAddress);
-                  $(this).find('td.total div').each(function() {
-                      _refresh(baseAddress, $(this), true);
-                  });
-              });
+        $.fn.occMetrics = function(baseUrl){
+          this.each(function() {
+            var baseAddress = $(this).attr("data-address");
+            //console.debug(baseAddress);
+            $(this).find('td.total div').each(function() {
+              _refresh(baseAddress, $(this), true);
+            });
+          });
 
-              function _refresh(baseAddress, target, nest) {
-                  var $target = $(target);
+          function _refresh(baseAddress, target, nest) {
+            var $target = $(target);
 
-                  // always add the datasetKey to the cube address
-                  var address = "?" + baseAddress;
+            // always add the datasetKey to the cube address
+            var address = "?" + baseAddress;
 
-                  if ($target.closest("tr").attr("data-kingdom") != null) {
-                      address = address + "&taxonKey=" + $target.closest("tr").attr("data-kingdom");
-                  }
+            if ($target.closest("tr").attr("data-kingdom") != null) {
+              address = address + "&taxonKey=" + $target.closest("tr").attr("data-kingdom");
+            }
 
-                  if ($target.closest("td").attr("data-bor") != null) {
-                      address = address + "&basisOfRecord=" + $target.closest("td").attr("data-bor");
-                  }
+            if ($target.closest("td").attr("data-bor") != null) {
+              address = address + "&basisOfRecord=" + $target.closest("td").attr("data-bor");
+            }
 
-                  if ($target.hasClass("geo")) {
-                      address = address + "&isGeoreferenced=true";
-                  }
-                  var ws = baseUrl + 'occurrence/count' + address;
-                  //console.debug(ws);
-                  $.getJSON(ws + '&callback=?', function (data) {
-                      $(target).html(data);
-                      if (nest && data!=0) {
-                          // load the rest of the row
-                          $target.closest('tr').find('div').each(function() {
-                              _refresh(baseAddress, $(this), false);
-                          });
-                      } else if (nest) {
-                          // set the rest of the row to 0
-                          $target.closest('tr').find('div').each(function() {
-                              $(this).html("0");
-                          });
-                      }
-                  });
+            if ($target.hasClass("geo")) {
+              address = address + "&isGeoreferenced=true";
+            }
+            var ws = baseUrl + 'occurrence/count' + address;
+            //console.debug(ws);
+            $.getJSON(ws + '&callback=?', function (data) {
+              $(target).html(data);
+              if (nest && data!=0) {
+                // load the rest of the row
+                $target.closest('tr').find('div').each(function() {
+                  _refresh(baseAddress, $(this), false);
+                });
+              } else if (nest) {
+                // set the rest of the row to 0
+                $target.closest('tr').find('div').each(function() {
+                  $(this).html("0");
+                });
               }
-          };
+            });
+          }
+        };
       }
     }
 

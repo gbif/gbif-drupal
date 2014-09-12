88d43a05f56c5ffc59c8bacf25c8c9e8115f1529
Burke Chih-Jen Ko <burkeker@gmail.com>
2014-09-12 08:30:51 +0200

    Publications.

--- sites/all/modules/custom/gbifs/gbif_participant/theme/publications.tpl.php
+++ sites/all/modules/custom/gbifs/gbif_participant/theme/publications.tpl.php
@@ -3,12 +3,12 @@
       <section id="publication-intro" class="col-md-12 well well-lg well-margin-top clearfix"<?php print $attributes; ?>>
         <div class="row">
 					<header class="content-header col-md-8">
-						<h2>Uses of GBIF in scientific research</h2>
+						<h2><?php print $html['intro_title']; ?></h2>
 					</header>
         </div>
         <div class="row">
 					<div class="content content-full">
-
+						<?php print $html['intro_para']; ?>
 					</div>
         </div>
       </section>
@@ -16,11 +16,12 @@
 			<section id="publication-list" class="col-md-12 well well-lg">
 				<div class="row">
 					<header class="content-header col-md-12">
-						<h2>List of publications</h2>
+						<h2><?php print $html['list_title']; ?></h2>
 					</header>
 				</div>
 				<div class="row">
 					<div class="content content-full">
+						<?php print $html['list_publications']; ?>
 					</div>
 				</div>
 			</section>

1f01f1ba587a1742f60c03a2c96ce8c7db4ace74
Burke Chih-Jen Ko <burkeker@gmail.com>
2014-09-13 11:00:50 +0200

    RSS news items and publications should open a new window.

--- sites/all/modules/custom/gbifs/gbif_participant/theme/node-feed-item.tpl.php
+++ sites/all/modules/custom/gbifs/gbif_participant/theme/node-feed-item.tpl.php
@@ -1,7 +1,7 @@
 <ul>
 	<?php foreach ($feed_items as $k => $feed_item): ?>
 		<?php
-			$title_link = l($feed_item['title'], $feed_item['url']);
+			$title_link = l($feed_item['title'], $feed_item['url'], array('attributes' => array('target' => '_blank')));
 		?>
 			<li><?php print $title_link; ?>
 				<p><?php print $feed_item['timing']; ?></p>

6ffc7da92cd73f75f08f15ffdf90b11a2362dfab
Burke Chih-Jen Ko <bko@gbif.org>
2015-02-04 11:40:52 +0100

    Now toggle status is in cookie.

--- sites/all/modules/custom/gbifs/gbif_resource/js/resources-widgets.js
+++ sites/all/modules/custom/gbifs/gbif_resource/js/resources-widgets.js
@@ -1,12 +1,24 @@
 (function($) {
     Drupal.behaviors.featuredToggle = {
         attach: function (context, settings) {
+            if ($.cookie("featured-block-toggle") == "hidden") {
+                $("#featured-resources-toggle").html("Show");
+                $("#featured-resources-rows").hide();
+            }
+            else if ($.cookie("featured-block-toggle") == "shown") {
+                $("#featured-resources-toggle").html("Hide");
+                $("#featured-resources-rows").show();
+            }
+
             $("#featured-resources-toggle").click(function () {
                 if ($("#featured-resources-rows").is(":visible")) {
                     $(this).html("Show");
+                    $.cookie("featured-block-toggle", "hidden");
                 } else {
                     $(this).html("Hide");
+                    $.cookie("featured-block-toggle", "shown");
                 }
+                console.log($.cookie("featured-block-toggle"));
                 $("#featured-resources-rows").toggle(100);
             });
         }

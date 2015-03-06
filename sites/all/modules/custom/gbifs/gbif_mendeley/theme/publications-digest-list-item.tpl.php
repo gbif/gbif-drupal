00c617a5b414fdcf11219623be72b06809447e6d
Burke Chih-Jen Ko <bko@gbif.org>
2015-03-06 15:05:22 +0100

    Changed module name to gbif_mendeley.

---
+++ sites/all/modules/custom/gbifs/gbif_mendeley/theme/publications-digest-list-item.tpl.php
@@ -0,0 +1,36 @@
+<ul class="publication-list">
+	<?php foreach ($publications as $k => $p): ?>
+	<?php
+		$title_link = l($p['title'], $p['websites'][0], array('attributes' => array('target' => '_blank')));
+
+		$title_link .= (substr($p['title'], -1) <> '?' || substr($p['title'], -1) <> '.') ? '. ' : '' ;
+
+		// Authors
+		$authors = '';
+		for ($i = 0; $i < count($p['authors']); $i++) {
+			$authors .= $p['authors'][$i]['last_name'] . ', ';
+			$authors .= strtoupper(substr($p['authors'][$i]['first_name'], 0, 1)) . '.';
+			if ($i < count($p['authors']) - 1) {
+				$authors .= ', ';
+			}
+		}
+		$year = isset($p['year']) ? ', ' . $p['year'] . '.' : NULL;
+
+		$journal = '';
+
+		if (isset($p['source']) && isset($p['volume']) && isset($p['issue']) && isset($p['pages'])) {
+			$journal .= (!empty($p['source'])) ? '<em>' . $p['source'] . '</em>' : '';
+			$journal .= (!empty($p['volume'])) ? ' ' . $p['volume'] : '';
+			$journal .= (!empty($p['issue'])) ? '(' . $p['issue'] . ')' : '';
+			$journal .= (!empty($p['pages'])) ? ' ' . $p['pages'] . '.' : '. ';
+		}
+		else {
+			$journal .= '';
+		}
+
+	?>
+	<li>
+		<strong><?php print $title_link; ?></strong> <?php print $authors . $year; ?> <?php print $journal; ?>
+	</li>
+	<?php endforeach; ?>
+</ul>

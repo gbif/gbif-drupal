488a1234f0cbce46a3aded61f1f2d661f091abac
Burke Chih-Jen Ko <burkeker@gmail.com>
2015-05-14 22:54:30 +0200

    The latest data publisher block in the resource tab of publishing-data section as an AngularJS app, including template updates.

--- sites/all/modules/custom/gbifs/gbif_resource/theme/publishing-data--resources.tpl.php
+++ sites/all/modules/custom/gbifs/gbif_resource/theme/publishing-data--resources.tpl.php
@@ -2,6 +2,9 @@
 /**
  * @file template file for the resource tab in publishing-data/ section.
  */
+
+$env = variable_get('environment_settings');
+
 ?>
 <div class="block-publishing-data-resources">
   <?php print render(module_invoke('views','block_view','resources-block_docs')); ?>
@@ -10,3 +13,23 @@
 <div class="block-publishing-data-resources">
   <?php print render(module_invoke('views','block_view','resources-block_tools')); ?>
 </div>
+
+<div ng-app="lpApp" class="block-publishing-data-resources" >
+  <div id="latest-publisher-container" ng-controller="lpController as lpCtrl">
+    <header>
+      <h2 class="featured-resources">Latest data publishers</h2>
+    </header>
+    <section id="featured-resources-rows">
+			<div class="views-row" ng-repeat="org in lpCtrl.orgs">
+				<div class="views-field views-field-title"><h3 class="field-content"><a href="{{org.portalUrl}}">{{org.title}}</a></h3></div>
+				<div class="views-field views-field-body">
+					<p class="field-content">{{org.summary}}</p>
+					<p ng-bind-html="org.endorsedText"></p>
+				</div>
+				<div class="views-field"><img class="org-logo" src="{{org.logoUrl}}"></div>
+			</div>
+      <p class="more text-right"><a href="<?php print $env['data_portal_base_url']; ?>/publisher/search">more data publishers</a></p>
+    </section>
+  </div>
+</div>
+

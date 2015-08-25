c0aacb3b12e1a8088a058c945d4fda5521aee19e
Burke Chih-Jen Ko <bko@gbif.org>
2015-08-25 11:09:20 +0200

    gbif_scaled_contents with new news content type.

---
+++ sites/all/modules/custom/gbifs/gbif_scaled_contents/migration/release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Put this file under the site folder to ensure drush get the correct configuration.
+
+drush features-revert -y gbif_vocabularies
+drush en -y gbif_scaled_contents
+drush features-revert -y gbif_scaled_contents
+
+# taxonomy terms migration
+
+# field deletions
+drush field-delete field_rhimage
+drush field-delete field_audiovideo
+drush field-delete field_publication
+drush field-delete field_opportunities
+
+# taxonomy terms deletion
+# taxonomy vocabularies deletion
+drush -v eval 'foreach(taxonomy_get_tree(22) as $term) { taxonomy_term_delete($term->tid); }'

b6dbb84d0934694f1e5df9a579f8fae5bbf89665
Burke Chih-Jen Ko <bko@gbif.org>
2015-08-27 14:57:05 +0200

    Finished mapping.

---
+++ sites/all/modules/custom/gbifs/gbif_scaled_contents/taxonomy_terms_mapping.php
@@ -0,0 +1,407 @@
+<?php
+
+$thematic_tags = array(
+  array(
+    'name' => 'Data use',
+    'old_tid' => 698,
+    'new_tid' => NULL,
+  ),
+  array(
+    'name' => 'Policy uses',
+    'old_tid' => 699,
+    'new_tid' => 898,
+  ),
+  array(
+    'name' => 'Science uses',
+    'old_tid' => 700,
+    'new_tid' => 1062,
+  ),
+  array(
+    'name' => 'Agriculture/biofuels',
+    'old_tid' => 701,
+    'new_tid' => 891,
+  ),
+  array(
+    'name' => 'Climate change',
+    'old_tid' => 702,
+    'new_tid' => 970,
+  ),
+  array(
+    'name' => 'Conservation',
+    'old_tid' => 703,
+    'new_tid' => 826,
+  ),
+  array(
+    'name' => 'Freshwater',
+    'old_tid' => 709,
+    'new_tid' => 887,
+  ),
+  array(
+    'name' => 'Human health',
+    'old_tid' => 704,
+    'new_tid' => 878,
+  ),
+  array(
+    'name' => 'Invasives',
+    'old_tid' => 705,
+    'new_tid' => 968,
+  ),
+  array(
+    'name' => 'Marine',
+    'old_tid' => 710,
+    'new_tid' => 861,
+  ),
+  array(
+    'name' => 'Mountain biodiversity',
+    'old_tid' => 707,
+    'new_tid' => 835,
+  ),
+  array(
+    'name' => 'Species distributions',
+    'old_tid' => 708,
+    'new_tid' => 1038,
+  ),
+  array(
+    'name' => 'Strategic applications',
+    'old_tid' => 711,
+    'new_tid' => 1004,
+  ),
+  array(
+    'name' => 'About GBIF',
+    'old_tid' => 691,
+    'new_tid' => NULL,
+  ),
+  array(
+    'name' => 'GBIF awards',
+    'old_tid' => 697,
+    'new_tid' => 587,
+  ),
+  array(
+    'name' => 'GBIF governance',
+    'old_tid' => 693,
+    'new_tid' => 584,
+  ),
+  array(
+    'name' => 'GBIF history/overview',
+    'old_tid' => 692,
+    'new_tid' => 583,
+  ),
+  array(
+    'name' => 'GBIF nodes',
+    'old_tid' => 695,
+    'new_tid' => 585,
+  ),
+  array(
+    'name' => 'GBIF partnerships',
+    'old_tid' => 696,
+    'new_tid' => 586,
+  ),
+  array(
+    'name' => 'GBIF regions',
+    'old_tid' => 694,
+    'new_tid' => 589,
+  ),
+  array(
+    'name' => 'Data access and retrieval',
+    'old_tid' => 685,
+    'new_tid' => 991,
+  ),
+  array(
+    'name' => 'GBIF.org',
+    'old_tid' => 686,
+    'new_tid' => 991,
+  ),
+  array(
+    'name' => 'National/thematic portals',
+    'old_tid' => 687,
+    'new_tid' => 893,
+  ),
+  array(
+    'name' => 'Nodes Portal Toolkit',
+    'old_tid' => 688,
+    'new_tid' => 893,
+  ),
+  array(
+    'name' => 'Web services',
+    'old_tid' => 689,
+    'new_tid' => 991,
+  ),
+  array(
+    'name' => 'Data publishing',
+    'old_tid' => 673,
+    'new_tid' => 955,
+  ),
+  array(
+    'name' => 'Academic publishing',
+    'old_tid' => 674,
+    'new_tid' => 955,
+  ),
+  array(
+    'name' => 'Data citation',
+    'old_tid' => 680,
+    'new_tid' => 955,
+  ),
+  array(
+    'name' => 'Data mobilization',
+    'old_tid' => 676,
+    'new_tid' => 955,
+  ),
+  array(
+    'name' => 'Data papers',
+    'old_tid' => 675,
+    'new_tid' => 955,
+  ),
+  array(
+    'name' => 'Data publishing software',
+    'old_tid' => 677,
+    'new_tid' => 955,
+  ),
+  array(
+    'name' => 'Integrated Publishing Toolkit',
+    'old_tid' => 678,
+    'new_tid' => 955,
+  ),
+  array(
+    'name' => 'Incentives',
+    'old_tid' => 679,
+    'new_tid' => 955,
+  ),
+  array(
+    'name' => 'Intellectual property rights',
+    'old_tid' => 681,
+    'new_tid' => 955,
+  ),
+  array(
+    'name' => 'Data management/curation',
+    'old_tid' => 668,
+    'new_tid' => 938,
+  ),
+  array(
+    'name' => 'Data management software',
+    'old_tid' => 669,
+    'new_tid' => 938,
+  ),
+  array(
+    'name' => 'Data quality',
+    'old_tid' => 670,
+    'new_tid' => 938,
+  ),
+  array(
+    'name' => 'Georeferencing',
+    'old_tid' => 671,
+    'new_tid' => 938,
+  ),
+  array(
+    'name' => 'Sensitive data',
+    'old_tid' => 672,
+    'new_tid' => 938,
+  ),
+  array(
+    'name' => 'Data capture and digitization',
+    'old_tid' => 660,
+    'new_tid' => 871,
+  ),
+  array(
+    'name' => 'Citizen science',
+    'old_tid' => 664,
+    'new_tid' => 871,
+  ),
+  array(
+    'name' => 'Collections',
+    'old_tid' => 663,
+    'new_tid' => 871,
+  ),
+  array(
+    'name' => 'Data capture software',
+    'old_tid' => 661,
+    'new_tid' => 871,
+  ),
+  array(
+    'name' => 'Digitization',
+    'old_tid' => 662,
+    'new_tid' => 871,
+  ),
+  array(
+    'name' => 'Biodiversity data types',
+    'old_tid' => 652,
+    'new_tid' => NULL,
+  ),
+  array(
+    'name' => 'Abundance data',
+    'old_tid' => 682,
+    'new_tid' => 1233,
+  ),
+  array(
+    'name' => 'Ecological/ecosystem data',
+    'old_tid' => 657,
+    'new_tid' => 833,
+  ),
+  array(
+    'name' => 'Genetic/genomic data',
+    'old_tid' => 658,
+    'new_tid' => 952,
+  ),
+  array(
+    'name' => 'Metadata',
+    'old_tid' => 653,
+    'new_tid' => 1002,
+  ),
+  array(
+    'name' => 'Multimedia data',
+    'old_tid' => 659,
+    'new_tid' => 919,
+  ),
+  array(
+    'name' => 'Names and checklists',
+    'old_tid' => 655,
+    'new_tid' => 984,
+  ),
+  array(
+    'name' => 'Occurrence data',
+    'old_tid' => 654,
+    'new_tid' => 834,
+  ),
+  array(
+    'name' => 'Species-level data',
+    'old_tid' => 656,
+    'new_tid' => 825,
+  ),
+  array(
+    'name' => 'Capacity',
+    'old_tid' => 647,
+    'new_tid' => NULL,
+  ),
+  array(
+    'name' => 'Capacity projects',
+    'old_tid' => 588,
+    'new_tid' => 866,
+  ),
+  array(
+    'name' => 'Mentoring',
+    'old_tid' => 546,
+    'new_tid' => 841,
+  ),
+  array(
+    'name' => 'Training',
+    'old_tid' => 545,
+    'new_tid' => 1058,
+  ),
+  array(
+    'name' => 'Informatics',
+    'old_tid' => 648,
+    'new_tid' => NULL,
+  ),
+  array(
+    'name' => 'Architecture',
+    'old_tid' => 649,
+    'new_tid' => 1050,
+  ),
+  array(
+    'name' => 'Persistent identifiers',
+    'old_tid' => 651,
+    'new_tid' => 1234,
+  ),
+  array(
+    'name' => 'Standards and protocols',
+    'old_tid' => 650,
+    'new_tid' => 999,
+  ),
+  array(
+    'name' => 'Vocabularies',
+    'old_tid' => 684,
+    'new_tid' => 1235,
+  ),
+  array(
+    'name' => 'Informatics tools',
+    'old_tid' => 683,
+    'new_tid' => NULL,
+  ),
+);
+
+$data_use_catagories = array(
+  array(
+    'name' => 'Policy uses',
+    'old_tid' => 618,
+    'new_tid' => 898,
+  ),
+  array(
+    'name' => 'Science uses',
+    'old_tid' => 617,
+    'new_tid' => 1062,
+  ),
+  array(
+    'name' => 'Advancing science',
+    'old_tid' => 713,
+    'new_tid' => NULL,
+  ),
+  array(
+    'name' => 'Biogeography',
+    'old_tid' => 715,
+    'new_tid' => 1048,
+  ),
+  array(
+    'name' => 'Ecology',
+    'old_tid' => 714,
+    'new_tid' => 836,
+  ),
+  array(
+    'name' => 'Evolution',
+    'old_tid' => 716,
+    'new_tid' => 951,
+  ),
+  array(
+    'name' => 'Taxonomic research',
+    'old_tid' => 717,
+    'new_tid' => 928,
+  ),
+  array(
+    'name' => 'Agriculture',
+    'old_tid' => 636,
+    'new_tid' => 891,
+  ),
+  array(
+    'name' => 'Climate change',
+    'old_tid' => 639,
+    'new_tid' => 970,
+  ),
+  array(
+    'name' => 'Conservation',
+    'old_tid' => 638,
+    'new_tid' => 826,
+  ),
+  array(
+    'name' => 'Human health',
+    'old_tid' => 637,
+    'new_tid' => 878,
+  ),
+  array(
+    'name' => 'Invasives',
+    'old_tid' => 635,
+    'new_tid' => 968,
+  ),
+  array(
+    'name' => 'Mountain biodiversity',
+    'old_tid' => 690,
+    'new_tid' => 835,
+  ),
+  array(
+    'name' => 'Species distributions',
+    'old_tid' => 640,
+    'new_tid' => 1038,
+  ),
+  array(
+    'name' => 'Freshwater',
+    'old_tid' => 643,
+    'new_tid' => 887,
+  ),
+  array(
+    'name' => 'Marine',
+    'old_tid' => 641,
+    'new_tid' => 861,
+  ),
+  array(
+    'name' => 'Strategic applications',
+    'old_tid' => 621,
+    'new_tid' => 1004,
+  ),
+);

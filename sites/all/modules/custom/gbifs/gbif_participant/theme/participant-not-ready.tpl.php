4b3b92577595e3c5a3eb784d0259d0eaf6baeb95
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-06-01 12:09:30 +0200

    Hide the participation tab for former participants.

--- sites/all/modules/custom/gbifs/gbif_participant/theme/participant-not-ready.tpl.php
+++ sites/all/modules/custom/gbifs/gbif_participant/theme/participant-not-ready.tpl.php
@@ -1,17 +1,17 @@
 <?php
-  drupal_set_title($title . ' - ' . t('Participation in GBIF'));
+  drupal_set_title(t('Participation in GBIF'));
 ?>
 <article id="participation" class="container">
 	<div class="row">
       <section id="news" class="col-xs-12 well well-lg well-margin-top clearfix"<?php print $attributes; ?>>
         <div class="row">
 					<header class="content-header col-xs-8">
-						<h2>Participation information is being updated...</h2>
+						<h2>Participation information not available</h2>
 					</header>
         </div>
         <div class="row">
 					<div class="content flat-list col-xs-8">
-						<p>We're busy updating participation information. Please come back later.</p>
+						<p>It may be because of the membership status of the country, or there are information pending before we can show them. Please contact us for the status of this page. Thank you.</p>
 					</div>
         </div>
       </section>

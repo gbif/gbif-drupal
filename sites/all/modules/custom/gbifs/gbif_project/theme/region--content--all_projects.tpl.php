0e78664a5ed0fc06e0badba2b67f8c54f18d387e
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-06-21 21:29:33 +0200

    PF-2536 Yellow bar and highlighted menu now reflect the umbralla programme. Views contextual URL is wired with programme acronym.

--- sites/all/modules/custom/gbifs/gbif_project/theme/region--content--all_projects.tpl.php
+++ sites/all/modules/custom/gbifs/gbif_project/theme/region--content--all_projects.tpl.php
@@ -1,25 +1,7 @@
 <?php
-
 /**
  * @file
  * Default theme implementation to display a region.
- *
- * Available variables:
- * - $content: The content for this region, typically blocks.
- * - $classes: String of classes that can be used to style contextually through
- *   CSS. It can be manipulated through the variable $classes_array from
- *   preprocess functions. The default values can be one or more of the following:
- *   - region: The current template type, i.e., "theming hook".
- *   - region-[name]: The name of the region with underscores replaced with
- *     dashes. For example, the page_top region would have a region-page-top class.
- * - $region: The name of the region variable as defined in the theme's .info file.
- *
- * Helper variables:
- * - $classes_array: Array of html class attribute values. It is flattened
- *   into a string within the variable $classes.
- * - $is_admin: Flags true when the current user is an administrator.
- * - $is_front: Flags true when presented in the front page.
- * - $logged_in: Flags true when the current user is a logged-in member.
  *
  * @see template_preprocess()
  * @see template_preprocess_region()
@@ -27,11 +9,24 @@
  *
  * @ingroup themeable
  */
+
+$back_menu_url = 'programme/capacity-support';
+$back_bar_title = t('Back to Capacity enhancement support programme');
+switch (arg(1)) {
+  case 'bid':
+    $back_menu_url = 'programme/bid';
+    $back_bar_title = t('Back to Biodiversity Information for Development');
+    break;
+  case 'bifa':
+    $back_menu_url = 'programme/bifa';
+    $back_bar_title = t('Back to Biodiversity Information Fund for Asia');
+    break;
+}
 ?>
 <div class="back-to-previous">
   <div class="container">
     <div class="row">
-      <p>&lt; <u><a href="/programme/capacity-support">Back to Capacity enhancement support programme</a></u></p>
+      <p>&lt; <u><a href="/<?php print $back_menu_url; ?>"><?php print $back_bar_title; ?></a></u></p>
     </div>
   </div>
 </div>

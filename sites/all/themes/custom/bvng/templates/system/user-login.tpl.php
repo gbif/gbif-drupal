335db1d7bee80fec7040d1350a36fa87374da994
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-10-06 15:17:52 +0200

    Prompt for the change of license agreement.

--- sites/all/themes/custom/bvng/templates/system/user-login.tpl.php
+++ sites/all/themes/custom/bvng/templates/system/user-login.tpl.php
@@ -19,9 +19,18 @@
         </div>
       </div>
       <div class="row user-login-action">
-          <?php print drupal_render($form['actions']); ?>
-          <p><a href="/user/password">Forgot your password?</a></p>
-          <p>Do you need to sign up? <a href="/user/register">Create your account.</a></p>
+        <div class="row">
+          <div class="col-xs-8">
+            <div>
+              <p>By logging in to GBIF.org you agree to the <a href="/terms/privacy-policy">privacy agreement</a>.  Please be aware that this was changed on October 6th, 2016.</p>
+            </div>
+            <div class="forgot-password">
+              <p><a href="/user/password">Forgot your password?</a></p>
+              <p>Do you need to sign up? <a href="/user/register">Create your account.</a></p>
+            </div>
+          </div>
+        </div>
+        <?php print drupal_render($form['actions']); ?>
       </div>
 
     </div>
